ab9bb4d034d57e917edab78d8c13114f16418055
Flier Lu <flier.lu@gmail.com>
2019-03-07 12:51:21 +0800

    use cfg-if instead of #[cfg]

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,13 @@
 //!  The t1ha was originally developed by Leonid Yuriev (Леонид Юрьев)
 //!  for The 1Hippeus project - zerocopy messaging in the spirit of Sparta!
 #![no_std]
+
 #[cfg(any(test, feature = "std"))]
 extern crate std;
+#[macro_use]
+extern crate cfg_if;
 
 use core::hash::{BuildHasherDefault, Hasher};
-#[cfg(feature = "std")]
-use std::collections::{HashMap, HashSet};
 
 mod bits;
 mod nightly;
@@ -32,45 +33,40 @@ pub use t1ha0::{t1ha0_32be, t1ha0_32le};
 pub use t1ha1::{t1ha1_be, t1ha1_le};
 pub use t1ha2::{t1ha2_atonce, t1ha2_atonce128, T1ha2Hasher};
 
-#[cfg(target_endian = "little")]
-pub use t1ha0::t1ha0_32le as t1ha0_32;
+cfg_if! {
+    if #[cfg(target_endian = "little")] {
+        pub use t1ha0::t1ha0_32le as t1ha0_32;
+        pub use t1ha1::t1ha1_le as t1ha1;
+    } else {
+        pub use t1ha0::t1ha0_32be as t1ha0_32;
+        pub use t1ha1::t1ha1_be as t1ha1;
+    }
+}
 
-#[cfg(target_endian = "big")]
-pub use t1ha0::t1ha0_32be as t1ha0_32;
-
-#[cfg(target_endian = "little")]
-pub use t1ha1::t1ha1_le as t1ha1;
-
-#[cfg(target_endian = "big")]
-pub use t1ha1::t1ha1_be as t1ha1;
-
-#[cfg(all(
-    any(target_arch = "x86", target_arch = "x86_64"),
-    any(
-        target_feature = "aes",
-        target_feature = "avx",
-        target_feature = "avx2"
-    ),
-))]
-mod t1ha0_aes;
-
-#[cfg(all(
-    any(target_arch = "x86", target_arch = "x86_64"),
-    target_feature = "aes",
-))]
-pub use t1ha0_aes::t1ha0_ia32aes as t1ha0_ia32aes_noavx;
-
-#[cfg(all(
-    any(target_arch = "x86", target_arch = "x86_64"),
-    target_feature = "avx"
-))]
-pub use t1ha0_aes::t1ha0_ia32aes as t1ha0_ia32aes_avx;
-
-#[cfg(all(
-    any(target_arch = "x86", target_arch = "x86_64"),
-    target_feature = "avx2"
-))]
-pub use t1ha0_aes::t1ha0_ia32aes_avx2;
+cfg_if! {
+    if #[cfg(any(target_arch = "x86", target_arch = "x86_64"))] {
+        cfg_if! {
+            if #[cfg(any(target_feature = "aes", target_feature = "avx", target_feature = "avx2"))] {
+                mod t1ha0_aes;
+            }
+        }
+        cfg_if! {
+            if #[cfg(target_feature = "aes")] {
+                pub use t1ha0_aes::t1ha0_ia32aes as t1ha0_ia32aes_noavx;
+            }
+        }
+        cfg_if! {
+            if #[cfg(target_feature = "avx")] {
+                pub use t1ha0_aes::t1ha0_ia32aes as t1ha0_ia32aes_avx;
+            }
+        }
+        cfg_if! {
+            if #[cfg(target_feature = "avx2")] {
+                pub use t1ha0_aes::t1ha0_ia32aes_avx2;
+            }
+        }
+    }
+}
 
 /// An implementation of the `t1ha` hash function.
 ///
@@ -101,13 +97,17 @@ impl Hasher for T1haHasher {
 /// A builder for default `t1ha` hashers.
 pub type T1haBuildHasher = BuildHasherDefault<T1haHasher>;
 
-/// A `HashMap` using a default `t1ha` hasher.
-#[cfg(feature = "std")]
-pub type T1haHashMap<K, V> = HashMap<K, V, T1haBuildHasher>;
+cfg_if! {
+    if #[cfg(feature = "std")] {
+        use std::collections::{HashMap, HashSet};
 
-/// A `HashSet` using a default `t1ha` hasher.
-#[cfg(feature = "std")]
-pub type T1haHashSet<T> = HashSet<T, T1haBuildHasher>;
+        /// A `HashMap` using a default `t1ha` hasher.
+        pub type T1haHashMap<K, V> = HashMap<K, V, T1haBuildHasher>;
+
+        /// A `HashSet` using a default `t1ha` hasher.
+        pub type T1haHashSet<T> = HashSet<T, T1haBuildHasher>;
+    }
+}
 
 /// `t1ha` library offers the t1ha0() function as the fastest for current CPU.
 ///
@@ -118,60 +118,47 @@ pub fn t1ha0(data: &[u8], seed: u64) -> u64 {
     prefer::HASH(data, seed)
 }
 
-#[cfg(not(all(feature = "runtime_select", feature = "std")))]
 mod prefer {
-    #[cfg(target_pointer_width = "64")]
-    pub use super::t1ha1 as HASH;
-
-    #[cfg(target_pointer_width = "32")]
-    pub use super::t1ha0_32 as HASH;
-}
-
-#[cfg(all(feature = "runtime_select", feature = "std"))]
-mod prefer {
-    use std::is_x86_feature_detected;
-
-    use lazy_static::lazy_static;
-
-    use super::*;
-
-    lazy_static! {
-        pub static ref HASH: fn(data: &[u8], seed: u64) -> u64 = t1ha0_resolve();
+    cfg_if! {
+        if #[cfg(target_pointer_width = "64")] {
+            pub use super::t1ha1 as t1ha0_fallback;
+        } else {
+            pub use super::t1ha0_32 as t1ha0_fallback;
+        }
     }
 
-    #[cfg(target_pointer_width = "64")]
-    pub use crate::t1ha1 as t1ha0_fallback;
+    cfg_if! {
+        if #[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), feature = "runtime_select", feature = "std"))] {
+            use std::is_x86_feature_detected;
 
-    #[cfg(target_pointer_width = "32")]
-    pub use crate::t1ha0_32 as t1ha0_fallback;
+            use lazy_static::lazy_static;
 
-    #[cfg(all(
-        any(target_arch = "x86", target_arch = "x86_64"),
-        not(target_feature = "aes"),
-    ))]
-    pub use self::t1ha0_fallback as t1ha0_ia32aes_noavx;
+            lazy_static! {
+                pub static ref HASH: fn(data: &[u8], seed: u64) -> u64 = t1ha0_resolve();
+            }
 
-    #[cfg(all(
-        any(target_arch = "x86", target_arch = "x86_64"),
-        not(target_feature = "avx")
-    ))]
-    pub use self::t1ha0_fallback as t1ha0_ia32aes_avx;
+            #[cfg(not(target_feature = "aes"))]
+            pub use self::t1ha0_fallback as t1ha0_ia32aes_noavx;
 
-    #[cfg(all(
-        any(target_arch = "x86", target_arch = "x86_64"),
-        not(target_feature = "avx2")
-    ))]
-    pub use self::t1ha0_fallback as t1ha0_ia32aes_avx2;
+            #[cfg(not(target_feature = "avx"))]
+            pub use self::t1ha0_fallback as t1ha0_ia32aes_avx;
 
-    fn t1ha0_resolve() -> fn(&[u8], u64) -> u64 {
-        if is_x86_feature_detected!("avx2") {
-            t1ha0_ia32aes_avx2
-        } else if is_x86_feature_detected!("avx") {
-            t1ha0_ia32aes_avx
-        } else if is_x86_feature_detected!("aes") {
-            t1ha0_ia32aes_noavx
+            #[cfg(not(target_feature = "avx2"))]
+            pub use self::t1ha0_fallback as t1ha0_ia32aes_avx2;
+
+            fn t1ha0_resolve() -> fn(&[u8], u64) -> u64 {
+                if is_x86_feature_detected!("avx2") {
+                    t1ha0_ia32aes_avx2
+                } else if is_x86_feature_detected!("avx") {
+                    t1ha0_ia32aes_avx
+                } else if is_x86_feature_detected!("aes") {
+                    t1ha0_ia32aes_noavx
+                } else {
+                    t1ha0_fallback
+                }
+            }
         } else {
-            t1ha0_fallback
+            pub use self::t1ha0_fallback as HASH;
         }
     }
 }
