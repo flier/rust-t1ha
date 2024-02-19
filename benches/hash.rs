046bbddda0c7ccf2e2ae895c4bf378cfbb4a242a
Flier Lu <flier.lu@gmail.com>
2024-02-19 16:26:12 +0800

    upgrade benchmark

--- benches/hash.rs
+++ benches/hash.rs
@@ -4,6 +4,8 @@
 extern crate lazy_static;
 #[macro_use]
 extern crate criterion;
+#[macro_use]
+extern crate cfg_if;
 
 use std::collections::hash_map::DefaultHasher;
 use std::hash::Hasher;
@@ -12,13 +14,13 @@ use std::io::BufReader;
 use std::mem;
 use std::slice;
 
-use criterion::{black_box, Criterion, ParameterizedBenchmark, Throughput};
+use criterion::{black_box, BenchmarkId, Criterion, Throughput};
 
 use ahash::AHasher;
 use farmhash::{hash32_with_seed as farmhash32, hash64_with_seed as farmhash64};
 use fnv::FnvHasher;
 use fxhash::{hash32 as fxhash32, hash64 as fxhash64};
-use meowhash::MeowHasher;
+use meowhash::{MeowHash, MeowHasher};
 use metrohash::{MetroHash128, MetroHash64};
 use murmur3::{murmur3_32, murmur3_x64_128, murmur3_x86_128};
 use rustc_hash::FxHasher;
@@ -27,236 +29,259 @@ use t1ha::{t1ha0_32, t1ha1, t1ha2_atonce, t1ha2_atonce128};
 use twox_hash::{XxHash as XxHash64, XxHash32};
 use xxhash2::{hash32 as xxhash32, hash64 as xxhash64};
 
-#[cfg(target_feature = "aes")]
-use t1ha::t1ha0_ia32aes_noavx;
-#[cfg(not(target_feature = "aes"))]
-fn t1ha0_ia32aes_noavx(_data: &[u8], _seed: u64) -> u64 {
-    0
+cfg_if! {
+    if #[cfg(target_feature = "aes")] {
+        use t1ha::t1ha0_ia32aes_noavx;
+    } else {
+        fn t1ha0_ia32aes_noavx(_data: &[u8], _seed: u64) -> u64 {
+            0
+        }
+    }
 }
 
-#[cfg(target_feature = "avx2")]
-use t1ha::t1ha0_ia32aes_avx2;
-#[cfg(not(target_feature = "avx2"))]
-fn t1ha0_ia32aes_avx2(_data: &[u8], _seed: u64) -> u64 {
-    0
+cfg_if! {
+    if #[cfg(target_feature = "avx")] {
+        use t1ha::t1ha0_ia32aes_avx;
+    } else {
+        fn t1ha0_ia32aes_avx(_data: &[u8], _seed: u64) -> u64 {
+            0
+        }
+    }
 }
 
-#[cfg(target_feature = "avx")]
-use t1ha::t1ha0_ia32aes_avx;
-#[cfg(not(target_feature = "avx"))]
-fn t1ha0_ia32aes_avx(_data: &[u8], _seed: u64) -> u64 {
-    0
+cfg_if! {
+    if #[cfg(target_feature = "avx2")] {
+        use t1ha::t1ha0_ia32aes_avx2;
+    } else {
+        fn t1ha0_ia32aes_avx2(_data: &[u8], _seed: u64) -> u64 {
+            0
+        }
+    }
 }
 
-const KB: usize = 1024;
+const KB: u64 = 1024;
 const SEED: u64 = 0x0123456789ABCDEF;
-const PARAMS: [usize; 7] = [7, 8, 32, 256, KB, 4 * KB, 16 * KB];
+const PARAMS: [u64; 7] = [7, 8, 32, 256, KB, 4 * KB, 16 * KB];
 
 lazy_static! {
     static ref DATA: Vec<u8> = (0..16 * KB).map(|b| b as u8).collect::<Vec<_>>();
 }
 
 fn bench_memory(c: &mut Criterion) {
-    c.bench(
-        "memory",
-        ParameterizedBenchmark::new(
-            "sum",
-            move |b, &&size| {
+    let mut group = c.benchmark_group("memory");
+
+    for size in PARAMS {
+        group.throughput(Throughput::Bytes(size)).bench_with_input(
+            BenchmarkId::from_parameter(size),
+            &size,
+            |b, _| {
                 let s = unsafe {
-                    slice::from_raw_parts(DATA.as_ptr() as *mut u32, size / mem::size_of::<u32>())
+                    slice::from_raw_parts(
+                        DATA.as_ptr().cast::<u32>(),
+                        size as usize / mem::size_of::<u32>(),
+                    )
                 };
 
                 b.iter(|| {
                     black_box(s.iter().fold(0u64, |acc, &x| acc + x as u64));
                 })
             },
-            &PARAMS,
-        )
-        .throughput(|&&size| Throughput::Bytes(size as u32)),
-    );
+        );
+    }
 }
 
 fn bench_hash32(c: &mut Criterion) {
-    c.bench(
-        "hash32",
-        ParameterizedBenchmark::new(
-            "t1ha0_32",
-            move |b, &&size| {
-                b.iter(|| t1ha0_32(&DATA[..size], SEED));
-            },
-            &PARAMS,
-        )
-        .with_function("murmur3_32", move |b, &&size| {
-            b.iter(|| {
-                let mut r = BufReader::new(&DATA[..size]);
+    let mut group = c.benchmark_group("hash32");
 
-                murmur3_32(&mut r, SEED as u32)
+    for size in PARAMS {
+        let data = &DATA[..size as usize];
+
+        group
+            .throughput(Throughput::Bytes(size))
+            .bench_with_input(BenchmarkId::new("t1ha0_32", size), &size, |b, _| {
+                b.iter(|| t1ha0_32(data, SEED));
+            })
+            .bench_with_input(BenchmarkId::new("murmur3_32", size), &size, |b, _| {
+                b.iter(|| murmur3_32(&mut BufReader::new(data), SEED as u32));
+            })
+            .bench_with_input(BenchmarkId::new("farmhash32", size), &size, |b, _| {
+                b.iter(|| farmhash32(data, SEED as u32));
+            })
+            .bench_with_input(BenchmarkId::new("xxhash32", size), &size, |b, _| {
+                b.iter(|| xxhash32(data, SEED as u32));
+            })
+            .bench_with_input(
+                BenchmarkId::new("twox_hash::XxHash32", size),
+                &size,
+                |b, _| {
+                    b.iter(|| {
+                        let mut h = XxHash32::with_seed(SEED as u32);
+                        h.write(data);
+                        h.finish()
+                    });
+                },
+            )
+            .bench_with_input(BenchmarkId::new("fxhash32", size), &size, |b, _| {
+                b.iter(|| fxhash32(data));
             });
-        })
-        .with_function("farmhash32", move |b, &&size| {
-            b.iter(|| farmhash32(&DATA[..size], SEED as u32));
-        })
-        .with_function("xxhash32", move |b, &&size| {
-            b.iter(|| xxhash32(&DATA[..size], SEED as u32));
-        })
-        .with_function("twox_hash::XxHash32", move |b, &&size| {
-            b.iter(|| {
-                let mut h = XxHash32::with_seed(SEED as u32);
-                h.write(&DATA[..size]);
-                h.finish()
-            });
-        })
-        .with_function("fxhash32", move |b, &&size| {
-            b.iter(|| fxhash32(&DATA[..size]));
-        })
-        .throughput(|&&size| Throughput::Bytes(size as u32)),
-    );
+    }
 }
 
 fn bench_hash64(c: &mut Criterion) {
-    let mut bench = ParameterizedBenchmark::new(
-        "t1ha1",
-        move |b, &&size| {
-            b.iter(|| t1ha1(&DATA[..size], SEED));
-        },
-        &PARAMS,
-    )
-    .with_function("t1ha2_atonce", move |b, &&size| {
-        b.iter(|| t1ha2_atonce(&DATA[..size], SEED));
-    });
+    let mut group = c.benchmark_group("hash64");
 
-    if cfg!(target_feature = "aes") {
-        bench = bench.with_function("t1ha0_ia32aes_noavx", move |b, &&size| {
-            b.iter(|| t1ha0_ia32aes_noavx(&DATA[..size], SEED));
-        });
-    }
-    if cfg!(target_feature = "avx") {
-        bench = bench.with_function("t1ha0_ia32aes_avx", move |b, &&size| {
-            b.iter(|| t1ha0_ia32aes_avx(&DATA[..size], SEED));
-        });
-    }
-    if cfg!(target_feature = "avx2") {
-        bench = bench.with_function("t1ha0_ia32aes_avx2", move |b, &&size| {
-            b.iter(|| t1ha0_ia32aes_avx2(&DATA[..size], SEED));
-        });
-    }
+    for size in PARAMS {
+        let data = &DATA[..size as usize];
 
-    c.bench(
-        "hash64",
-        bench
-            .with_function("hash_map::DefaultHasher", move |b, &&size| {
-                b.iter(|| {
-                    let mut h = DefaultHasher::new();
-                    h.write(&DATA[..size]);
-                    h.finish()
-                });
+        group
+            .throughput(Throughput::Bytes(size))
+            .bench_with_input(BenchmarkId::new("t1ha1", size), &size, |b, _| {
+                b.iter(|| t1ha1(data, SEED));
             })
-            .with_function("siphash", move |b, &&size| {
+            .bench_with_input(BenchmarkId::new("t1ha2_atonce", size), &size, |b, _| {
+                b.iter(|| t1ha2_atonce(data, SEED));
+            });
+
+        if cfg!(target_feature = "aes") {
+            group.bench_with_input(
+                BenchmarkId::new("t1ha0_ia32aes_noavx", size),
+                &size,
+                |b, _| b.iter(|| t1ha0_ia32aes_noavx(data, SEED)),
+            );
+        }
+
+        if cfg!(target_feature = "avx") {
+            group.bench_with_input(
+                BenchmarkId::new("t1ha0_ia32aes_avx", size),
+                &size,
+                |b, _| b.iter(|| t1ha0_ia32aes_avx(data, SEED)),
+            );
+        }
+
+        if cfg!(target_feature = "avx2") {
+            group.bench_with_input(
+                BenchmarkId::new("t1ha0_ia32aes_avx2", size),
+                &size,
+                |b, _| b.iter(|| t1ha0_ia32aes_avx2(data, SEED)),
+            );
+        }
+
+        group
+            .bench_with_input(
+                BenchmarkId::new("hash_map::DefaultHasher", size),
+                &size,
+                |b, _| {
+                    b.iter(|| {
+                        let mut h = DefaultHasher::new();
+                        h.write(data);
+                        h.finish()
+                    });
+                },
+            )
+            .bench_with_input(BenchmarkId::new("siphash", size), &size, |b, _| {
                 b.iter(|| {
                     let mut h = SipHasher::new_with_keys(SEED, SEED);
-                    h.write(&DATA[..size]);
+                    h.write(data);
                     h.finish()
                 });
             })
-            .with_function("metrohash64", move |b, &&size| {
+            .bench_with_input(BenchmarkId::new("metrohash64", size), &size, |b, _| {
                 b.iter(|| {
                     let mut h = MetroHash64::with_seed(SEED);
-                    h.write(&DATA[..size]);
+                    h.write(data);
                     h.finish()
                 });
             })
-            .with_function("farmhash64", move |b, &&size| {
-                b.iter(|| farmhash64(&DATA[..size], SEED));
+            .bench_with_input(BenchmarkId::new("farmhash64", size), &size, |b, _| {
+                b.iter(|| farmhash64(data, SEED));
             })
-            .with_function("fnv64", move |b, &&size| {
+            .bench_with_input(BenchmarkId::new("fnv64", size), &size, |b, _| {
                 b.iter(|| {
                     let mut h = FnvHasher::with_key(SEED);
-                    h.write(&DATA[..size]);
+                    h.write(data);
                     h.finish()
                 });
             })
-            .with_function("xxhash64", move |b, &&size| {
-                b.iter(|| xxhash64(&DATA[..size], SEED));
+            .bench_with_input(BenchmarkId::new("xxhash64", size), &size, |b, _| {
+                b.iter(|| xxhash64(data, SEED));
             })
-            .with_function("twox_hash::XxHash", move |b, &&size| {
-                b.iter(|| {
-                    let mut h = XxHash64::with_seed(SEED);
-                    h.write(&DATA[..size]);
-                    h.finish()
-                });
+            .bench_with_input(
+                BenchmarkId::new("twox_hash::XxHash", size),
+                &size,
+                |b, _| {
+                    b.iter(|| {
+                        let mut h = XxHash64::with_seed(SEED);
+                        h.write(data);
+                        h.finish()
+                    });
+                },
+            )
+            .bench_with_input(BenchmarkId::new("seahash", size), &size, |b, _| {
+                b.iter(|| seahash64(data, SEED, SEED, SEED, SEED));
             })
-            .with_function("seahash", move |b, &&size| {
-                b.iter(|| seahash64(&DATA[..size], SEED, SEED, SEED, SEED));
+            .bench_with_input(BenchmarkId::new("fxhash64", size), &size, |b, _| {
+                b.iter(|| fxhash64(data));
             })
-            .with_function("fxhash64", move |b, &&size| {
-                b.iter(|| fxhash64(&DATA[..size]));
-            })
-            .with_function("ahash", move |b, &&size| {
+            .bench_with_input(BenchmarkId::new("ahash", size), &size, |b, _| {
                 b.iter(|| {
                     let mut h = AHasher::default();
-                    h.write(&DATA[..size]);
+                    h.write(data);
                     h.finish()
                 });
             })
-            .with_function("rustc_hash::FxHasher", move |b, &&size| {
-                b.iter(|| {
-                    let mut h = FxHasher::default();
-                    h.write(&DATA[..size]);
-                    h.finish()
-                });
-            })
-            .throughput(|&&size| Throughput::Bytes(size as u32)),
-    );
+            .bench_with_input(
+                BenchmarkId::new("rustc_hash::FxHasher", size),
+                &size,
+                |b, _| {
+                    b.iter(|| {
+                        let mut h = FxHasher::default();
+                        h.write(data);
+                        h.finish()
+                    });
+                },
+            );
+    }
 }
 
 fn bench_hash128(c: &mut Criterion) {
-    let mut bench = ParameterizedBenchmark::new(
-        "t1ha2_atonce128",
-        move |b, &&size| {
-            b.iter(|| t1ha2_atonce128(&DATA[..size], SEED));
-        },
-        &PARAMS,
-    )
-    .with_function("metrohash128", move |b, &&size| {
-        b.iter(|| {
-            let mut h = MetroHash128::with_seed(SEED);
-            h.write(&DATA[..size]);
-            h.finish128()
-        });
-    });
+    let mut group = c.benchmark_group("hash128");
 
-    if cfg!(target_arch = "x86_64") {
-        bench = bench.with_function("murmur3_x64_128", move |b, &&size| {
-            b.iter(|| {
-                let mut r = BufReader::new(&DATA[..size]);
-                let mut out = [0; 16];
+    for size in PARAMS {
+        let data = &DATA[..size as usize];
 
-                murmur3_x64_128(&mut r, SEED as u32, &mut out);
+        group
+            .throughput(Throughput::Bytes(size))
+            .bench_with_input(BenchmarkId::new("t1ha2_atonce128", size), &size, |b, _| {
+                b.iter(|| t1ha2_atonce128(data, SEED))
+            })
+            .bench_with_input(BenchmarkId::new("metrohash128", size), &size, |b, _| {
+                b.iter(|| {
+                    let mut h = MetroHash128::with_seed(SEED);
+                    h.write(data);
+                    h.finish128()
+                })
             });
-        });
-    }
 
-    if cfg!(target_arch = "x86") {
-        bench = bench.with_function("murmur3_x86_128", move |b, &&size| {
-            b.iter(|| {
-                let mut r = BufReader::new(&DATA[..size]);
-                let mut out = [0; 16];
-
-                murmur3_x86_128(&mut r, SEED as u32, &mut out);
+        if cfg!(target_arch = "x86_64") {
+            group.bench_with_input(BenchmarkId::new("murmur3_x64_128", size), &size, |b, _| {
+                b.iter(|| murmur3_x64_128(&mut BufReader::new(data), SEED as _))
             });
-        });
-    }
+        }
 
-    if cfg!(target_feature = "aes") {
-        bench = bench.with_function("meowhash128", move |b, &&size| {
-            b.iter(|| MeowHasher::digest_with_seed(SEED as u128, &DATA[..size]));
-        });
-    }
+        if cfg!(target_arch = "x86") {
+            group.bench_with_input(BenchmarkId::new("murmur3_x86_128", size), &size, |b, _| {
+                b.iter(|| murmur3_x86_128(&mut BufReader::new(data), SEED as _))
+            });
+        }
 
-    c.bench(
-        "hash128",
-        bench.throughput(|&&size| Throughput::Bytes(size as u32)),
-    );
+        if cfg!(target_feature = "aes") {
+            group.bench_with_input(BenchmarkId::new("meowhash128", size), &size, |b, _| {
+                b.iter(|| {
+                    MeowHasher::digest_with_seed(MeowHash::expand_seed(&SEED.to_ne_bytes()), data)
+                });
+            });
+        }
+    }
 }
 
 criterion_group!(
--- benches/t1ha.rs
+++ benches/t1ha.rs
@@ -2,120 +2,161 @@
 extern crate lazy_static;
 #[macro_use]
 extern crate criterion;
+#[macro_use]
+extern crate cfg_if;
 
 use std::mem;
 use std::slice;
 
-use criterion::{black_box, Criterion, ParameterizedBenchmark, Throughput};
+use criterion::{black_box, BenchmarkId, Criterion, Throughput};
 
 use t1ha::{t1ha0_32, t1ha1, t1ha2_atonce, t1ha2_atonce128, T1ha2Hasher};
 
-#[cfg(target_feature = "avx2")]
-use t1ha::t1ha0_ia32aes_avx2;
-#[cfg(not(target_feature = "avx2"))]
-fn t1ha0_ia32aes_avx2(_data: &[u8], _seed: u64) -> u64 {
-    0
+cfg_if! {
+    if #[cfg(target_feature = "aes")] {
+        use t1ha::t1ha0_ia32aes_noavx;
+    } else {
+        fn t1ha0_ia32aes_noavx(_data: &[u8], _seed: u64) -> u64 {
+            0
+        }
+    }
 }
 
-#[cfg(target_feature = "avx")]
-use t1ha::t1ha0_ia32aes_avx;
-#[cfg(not(target_feature = "avx"))]
-fn t1ha0_ia32aes_avx(_data: &[u8], _seed: u64) -> u64 {
-    0
+cfg_if! {
+    if #[cfg(target_feature = "avx")] {
+        use t1ha::t1ha0_ia32aes_avx;
+    } else {
+        fn t1ha0_ia32aes_avx(_data: &[u8], _seed: u64) -> u64 {
+            0
+        }
+    }
 }
 
-const KB: usize = 1024;
+cfg_if! {
+    if #[cfg(target_feature = "avx2")] {
+        use t1ha::t1ha0_ia32aes_avx2;
+    } else {
+        fn t1ha0_ia32aes_avx2(_data: &[u8], _seed: u64) -> u64 {
+            0
+        }
+    }
+}
+
+const KB: u64 = 1024;
 const SEED: u64 = 0x0123456789ABCDEF;
-const PARAMS: [usize; 11] = [7, 8, 32, 64, 256, 512, KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB];
+const PARAMS: [u64; 11] = [7, 8, 32, 64, 256, 512, KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB];
 
 lazy_static! {
     static ref DATA: Vec<u8> = (0..16 * KB).map(|b| b as u8).collect::<Vec<_>>();
 }
 
-fn bench_t1ha(c: &mut Criterion) {
-    c.bench(
-        "memory",
-        ParameterizedBenchmark::new(
-            "sum",
-            move |b, &&size| {
+fn bench_memory(c: &mut Criterion) {
+    let mut group = c.benchmark_group("memory");
+
+    for size in PARAMS {
+        group.throughput(Throughput::Bytes(size)).bench_with_input(
+            BenchmarkId::from_parameter(size),
+            &size,
+            |b, _| {
                 let s = unsafe {
-                    slice::from_raw_parts(DATA.as_ptr() as *mut u32, size / mem::size_of::<u32>())
+                    slice::from_raw_parts(
+                        DATA.as_ptr().cast::<u32>(),
+                        size as usize / mem::size_of::<u32>(),
+                    )
                 };
 
                 b.iter(|| {
                     black_box(s.iter().fold(0u64, |acc, &x| acc + x as u64));
                 })
             },
-            &PARAMS,
-        )
-        .throughput(|&&size| Throughput::Bytes(size as u32)),
-    );
-
-    let mut bench = ParameterizedBenchmark::new(
-        "t1ha0_32",
-        move |b, &&size| {
-            b.iter(|| t1ha0_32(&DATA[..size], SEED));
-        },
-        &PARAMS,
-    );
-
-    if cfg!(target_feature = "avx") {
-        bench = bench.with_function("t1ha0_ia32aes_avx", move |b, &&size| {
-            b.iter(|| t1ha0_ia32aes_avx(&DATA[..size], SEED));
-        });
+        );
     }
-    if cfg!(target_feature = "avx2") {
-        bench = bench.with_function("t1ha0_ia32aes_avx2", move |b, &&size| {
-            b.iter(|| t1ha0_ia32aes_avx2(&DATA[..size], SEED));
-        });
-    }
-
-    c.bench(
-        "t1ha0",
-        bench.throughput(|&&size| Throughput::Bytes(size as u32)),
-    );
-
-    c.bench(
-        "t1ha1",
-        ParameterizedBenchmark::new(
-            "t1ha1",
-            move |b, &&size| {
-                b.iter(|| t1ha1(&DATA[..size], SEED));
-            },
-            &PARAMS,
-        )
-        .throughput(|&&size| Throughput::Bytes(size as u32)),
-    );
-
-    c.bench(
-        "t1ha2",
-        ParameterizedBenchmark::new(
-            "t1ha2_atonce",
-            move |b, &&size| {
-                b.iter(|| t1ha2_atonce(&DATA[..size], SEED));
-            },
-            &PARAMS,
-        )
-        .with_function("t1ha2_atonce128", move |b, &&size| {
-            b.iter(|| t1ha2_atonce128(&DATA[..size], SEED));
-        })
-        .with_function("t1ha2_stream", move |b, &&size| {
-            b.iter(|| {
-                let mut h = T1ha2Hasher::with_seeds(SEED, SEED);
-                h.update(&DATA[..size]);
-                h.finish()
-            });
-        })
-        .with_function("t1ha2_stream128", move |b, &&size| {
-            b.iter(|| {
-                let mut h = T1ha2Hasher::with_seeds(SEED, SEED);
-                h.update(&DATA[..size]);
-                h.finish128() as u64
-            });
-        })
-        .throughput(|&&size| Throughput::Bytes(size as u32)),
-    );
 }
 
-criterion_group!(benches, bench_t1ha);
+fn bench_t1ha0(c: &mut Criterion) {
+    let mut group = c.benchmark_group("t1ha0");
+
+    for size in PARAMS {
+        let data = &DATA[..size as usize];
+
+        group.throughput(Throughput::Bytes(size)).bench_with_input(
+            BenchmarkId::new("t1ha0_32", size),
+            &size,
+            |b, _| {
+                b.iter(|| t1ha0_32(data, SEED));
+            },
+        );
+
+        if cfg!(target_feature = "aes") {
+            group.bench_with_input(
+                BenchmarkId::new("t1ha0_ia32aes_noavx", size),
+                &size,
+                |b, _| b.iter(|| t1ha0_ia32aes_noavx(data, SEED)),
+            );
+        }
+
+        if cfg!(target_feature = "avx") {
+            group.bench_with_input(
+                BenchmarkId::new("t1ha0_ia32aes_avx", size),
+                &size,
+                |b, _| b.iter(|| t1ha0_ia32aes_avx(data, SEED)),
+            );
+        }
+
+        if cfg!(target_feature = "avx2") {
+            group.bench_with_input(
+                BenchmarkId::new("t1ha0_ia32aes_avx2", size),
+                &size,
+                |b, _| b.iter(|| t1ha0_ia32aes_avx2(data, SEED)),
+            );
+        }
+    }
+}
+
+fn bench_t1ha1(c: &mut Criterion) {
+    let mut group = c.benchmark_group("t1ha1");
+
+    for size in PARAMS {
+        let data = &DATA[..size as usize];
+
+        group.throughput(Throughput::Bytes(size)).bench_with_input(
+            BenchmarkId::new("t1ha1", size),
+            &size,
+            |b, _| b.iter(|| t1ha1(data, SEED)),
+        );
+    }
+}
+
+fn bench_t1ha2(c: &mut Criterion) {
+    let mut group = c.benchmark_group("t1ha2");
+
+    for size in PARAMS {
+        let data = &DATA[..size as usize];
+
+        group
+            .throughput(Throughput::Bytes(size))
+            .bench_with_input(BenchmarkId::new("t1ha2_atonce", size), &size, |b, _| {
+                b.iter(|| t1ha2_atonce(data, SEED))
+            })
+            .bench_with_input(BenchmarkId::new("t1ha2_stream", size), &size, |b, _| {
+                b.iter(|| {
+                    let mut h = T1ha2Hasher::with_seeds(SEED, SEED);
+                    h.update(data);
+                    h.finish()
+                })
+            })
+            .bench_with_input(BenchmarkId::new("t1ha2_atonce128", size), &size, |b, _| {
+                b.iter(|| t1ha2_atonce128(data, SEED))
+            })
+            .bench_with_input(BenchmarkId::new("t1ha2_stream123", size), &size, |b, _| {
+                b.iter(|| {
+                    let mut h = T1ha2Hasher::with_seeds(SEED, SEED);
+                    h.update(data);
+                    h.finish128()
+                })
+            });
+    }
+}
+
+criterion_group!(benches, bench_memory, bench_t1ha0, bench_t1ha1, bench_t1ha2);
 criterion_main!(benches);
